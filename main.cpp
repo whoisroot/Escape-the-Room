2625af0f3a57e2b14e6323c8457e2883d55e8871
Lucas Magalhães <lucassmagalhaes@uol.com.br>
2016-06-25 17:44:15 -0300

    Testes feitos em Fila. ELA FUNCIONA
    No ramo master
    	.Fila.cpp.swp
    nada adicionado ao envio mas arquivos não registrados estão presentes (use "git add" to regist

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "ListaCadastral.h"
+#include "Fila.h"
 
 using namespace std;
 
 int main(){
+	cout << "Testando ListaCadastral\n" << endl;
 	ListaCadastral *teste = new ListaCadastral;
 	No* testando = new No;
 	No* testando2 = new No;
@@ -24,5 +26,24 @@ int main(){
 	cout << "b deu certo? " << b << "\nE c, deu errado? " << c << endl;
 	cout << "Head depois do remove: " << teste->head->ID << endl;
 	teste->ImprimeLista();
+
+	cout << "\n\n\nTestando fila\n" << endl;
+	Fila *teste2 = new Fila;
+	No_fila *testando4 = new No_fila;
+	No_fila *testando5 = new No_fila;
+	No_fila *testando6 = new No_fila;
+	testando4->ID = 6;
+	testando5->ID = 9;
+	testando6->ID = 8;
+	teste2->Insere(testando4);
+	teste2->Insere(testando5);
+	teste2->Insere(testando6);
+	cout << "ID_4: " << testando4->ID << " ID_5: " << testando5->ID << " ID_6: " << testando6->ID << endl;
+
+	teste2->Inicio();
+	int i;
+	for(i = 0; i < teste2->Size(); i++)
+	cout << "Objeto " << i << ": " << teste2->ElementoAtual() << endl;
+
 	return 0;
 }
